d13ad9e23254a80fa281c512bc97a66de8b93d57
Arrigoni Alessandro <arrigoni.alessandro@osdbsesto>
2021-12-03 16:00:24 +0100

    File2 con href

--- file2.php
+++ file2.php
@@ -1,22 +1,28 @@
 
 <?php
 echo '
-<t1>benvenuto</t1></br>
-<form action= file2.php method=post>
-dimmi un path: <input type = text name=path>
-<input type=submit value=submit> 
+<table align=center>
+<tr><th><t1>benvenuto</t1></th></tr>
+<tr><th><form action= file2.php method=post></th></tr>
+<tr><th>dimmi un path: <input type = text name=path></th></tr>
+<tr><th><input type=submit value=submit> </th></tr>
+</table>
 </form>
 ';
-if (!empty($_POST['path'])) {
-    $hn = opendir($_POST['path']);
-    while ($file = readdir($hn)) {
-        if (is_dir($_POST['path'] . '/' . $file)) {
-            echo "$file è una cartella</br>";
-        } else {
-            echo "$file è un file</br>";
-        }
-    }
-    closedir($hn);
+if(!empty($_POST['path'])){
+$content[] = scandir($_POST['path']);
+echo "<table align=center>";
+for($i=0; $i<count(scandir($_POST['path'])); $i++){
+if(is_dir($_POST['path']."/".scandir($_POST['path'])[$i])){
+
+    echo "<tr><th>DIR <a href=ardizzone.pietro.tave.osdb.it/".$_POST['path']."/".scandir($_POST['path'])[$i].">".scandir($_POST['path'])[$i]."</a></th></tr>";
+
+}else{
+      
+    echo "<tr><th><a href=".$_POST['path']."/".scandir($_POST['path'])[$i].">".scandir($_POST['path'])[$i]."</a></th></tr>";
+}
 }
 
+closedir($hn);
+}
 ?>
